6bbc5447e4f380605f16612ac33291aa414bbfd3
421752547@qq.com <421752547@qq.com>
2020-10-20 14:17:45 +0800

    Create TestAddClass.cs

---
+++ TestAddClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTest
+{
+    class TestAddClass
+    {
+        public void Test()
+        {
+            
+        }
+    }
+}
